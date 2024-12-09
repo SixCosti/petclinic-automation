b6cee5d314517f1703b9c188853a8a0a196230db
rocky <rocky@localhost.localdomain>
2024-12-09 20:13:49 +0000

    Ansible and Terraform basic setup, frontend api_url updated accordingly.

---
+++ petclinic-infra/outputs.tf
@@ -0,0 +1,3 @@
+output "public_ip" {
+  value = aws_instance.pet_clinic.public_ip
+}
--- spring-petclinic-angular/src/environments/environment.ts
+++ spring-petclinic-angular/src/environments/environment.ts
@@ -23,5 +23,5 @@
 
 export const environment = {
   production: false,
-  REST_API_URL: 'http://localhost:8080/petclinic/api/'
+  REST_API_URL: '/petclinic/api/'
 };
