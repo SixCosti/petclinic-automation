68a98ebb7c6811213c1aa87d1603e4f7122ae281
rocky <rocky@localhost.localdomain>
2024-12-17 22:07:12 +0000

    Zap tests

--- petclinic-infra/main.tf
+++ petclinic-infra/main.tf
@@ -67,36 +67,43 @@ resource "aws_security_group" "petclinic_sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
     from_port   = 9966
     to_port     = 9966
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.petclinic_sg.id]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535  # All TCP ports
-    protocol    = "tcp"
     cidr_blocks = [var.my_ip]
   }
+
+  # ingress {
+  #   from_port   = 0
+  #   to_port     = 65535  # All TCP ports
+  #   protocol    = "tcp"
+  #   cidr_blocks = [var.my_ip]
+  # }
 
   egress {
     from_port   = 0
